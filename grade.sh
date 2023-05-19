16e88b13b62767d99ca19d74fd5b8207020e1423
Jayden <thanhnghiep1007@gmail.com>
2023-05-19 16:55:18 -0700

    merge
    Initial commit

--- grade.sh
+++ grade.sh
@@ -1,4 +1,4 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='".\;..\lib\hamcrest-core-1.3.jar\;..\lib\junit-4.13.2.jar"'
 
 rm -rf student-submission
 rm -rf grading-area
@@ -11,6 +11,19 @@ echo 'Finished cloning'
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
+PATH=' ./grading-area/ListExamples.java'
+
+if [[ -f "./student-submission/ListExamples.java" ]]
+then
+    echo "Hello"
+    # cp student-submission/ListExamples.java grading-area
+    "C:\Program Files\Java\jdk-17.0.5\bin\javac" -cp $CPATH ./student-submission/ListExamples.java
+    echo "It worked!"
+else
+    echo "Invalid file"
+fi
+
+
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
